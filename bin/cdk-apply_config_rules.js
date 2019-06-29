8507c590863a964fc47f923525646a118a4f9c51
Homan13 <homan13@gmail.com>
2019-06-29 12:18:38 -0400

    Initial Commit

--- bin/cdk-apply_config_rules.js
+++ bin/cdk-apply_config_rules.js
@@ -3,7 +3,7 @@
 // @ts-ignore: Cannot find declaration file
 require('source-map-support/register');
 const cdk = require('@aws-cdk/core');
-const { CdkApplyConfigRulesStack } = require('../lib/cdk-apply_config_rules-stack');
+const { CdkApplyConfigRulesMgmtStack } = require('../lib/cdk-apply_config_rules_mgmt-stack');
 
 const app = new cdk.App();
-new CdkApplyConfigRulesStack(app, 'CdkApplyConfigRulesStack');
+new CdkApplyConfigRulesMgmtStack(app, 'CdkApplyConfigRulesMgmtStack');
