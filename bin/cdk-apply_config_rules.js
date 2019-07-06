c454dd1246a33526c0a7c908df3a954eb57d5e74
Homan13 <homan13@gmail.com>
2019-07-06 12:01:08 -0400

    v0.2.0 completed
    - IAM stack completed
    
    - Compute, DB, Storage and VPC stacks added to application

--- bin/cdk-apply_config_rules.js
+++ bin/cdk-apply_config_rules.js
@@ -5,7 +5,15 @@ require('source-map-support/register');
 const cdk = require('@aws-cdk/core');
 const { CdkApplyConfigRulesMgmtStack } = require('../lib/cdk-apply_config_rules_mgmt-stack');
 const { CdkApplyConfigRulesIAMStack } = require('../lib/cdk-apply_config_rules_iam-stack');
+const { CdkApplyConfigRulesComputeStack } = require('../lib/cdk-apply_config_rules_compute-stack');
+const { CdkApplyConfigRulesDBStack } = require('../lib/cdk-apply_config_rules_db-stack');
+const { CdkApplyConfigRulesStorageStack } = require('../lib/cdk-apply_config_rules_storage-stack');
+const { CdkApplyConfigRulesVPCStack } = require('../lib/cdk-apply_config_rules_vpc-stack');
 
 const app = new cdk.App();
 new CdkApplyConfigRulesMgmtStack(app, 'CdkApplyConfigRulesMgmtStack');
 new CdkApplyConfigRulesIAMStack(app, 'CdkApplyConfigRulesIAMStack');
+new CdkApplyConfigRulesComputeStack(app, 'CdkApplyConfigRulesComputeStack');
+new CdkApplyConfigRulesDBStack(app, 'CdkApplyConfigRulesDBStack');
+new CdkApplyConfigRulesStorageStack(app, 'CdkApplyConfigRulesStorageStack');
+new CdkApplyConfigRulesVPCStack(app, 'CdkApplyConfigRulesVPCStack');
